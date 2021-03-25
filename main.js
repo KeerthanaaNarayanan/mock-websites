b876e867aa0e7bb2dee8bb3e4bf54c3a7ce15081
Keerthanaa Narayanan <47668717+KeerthanaaNarayanan@users.noreply.github.com>
2021-03-26 00:45:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+'use strict'
+
+console.log(document.querySelector('#read-more').textContent);
+document.querySelector('#read-more').textContent = 'know more';
+
+document.querySelector('#send-button').addEventListener('click', function() {
+    console.log(document.querySelector('#name').value);
+    console.log(document.querySelector('#email').value);
+    console.log(document.querySelector('#phone').value);
+    console.log(document.querySelector('#message').value);
+});
+
+
+
+
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 42.361145, lng: -71.057083 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
